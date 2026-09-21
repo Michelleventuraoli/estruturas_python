7538d7db7298ec2ea4fabc272da19148b88e4af3
Michelleventuraoli <michelleventuranutri@gmail.com>
2026-09-21 12:52:28 -0300

    michelle ok

---
+++ michelleaula6.py
@@ -0,0 +1,19 @@
+# Recebe o nome do usúario na variável
+nome = input('Qual seu nome?')
+
+
+# Recebe o idade do usúario na variável
+age = input('Qual seu idade?')
+
+print()
+
+print(f'Olá {nome}, você tem {age} anos!')
+
+
+
+
+
+
+
+
+
